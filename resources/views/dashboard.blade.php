7907c247b0aed60061b062411cb16f77d8898e3f
ngTamAn111103 <ng.t.ann2003@gmail.com>
2023-04-12 23:13:10 +0700

    test

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,114 @@
+
+{{-- dashboard cũ --}}
+
+{{-- <!DOCTYPE html>
+<html>
+
+<head>
+    <title>Custom Auth in Laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
+        <div class="container">
+            <a class="navbar-brand mr-auto" href="#">PositronX</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register-user') }}">Register</a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('signout') }}">Logout</a>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
+</body>
+
+</html> --}}
+
+
+{{-- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ --}}
+
+
+{{-- Tâm An: 12/4/23
+    Dashboard mới đẹp hơn hông nè :v --}}
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Navigate Tomato Pizza</title>
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" id="dark-mode-general-link">
+    <link type="text/css" rel="stylesheet" id="dark-mode-custom-link">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="/resources/css/app.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #004666; color: aliceblue;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Tomato Pizza</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item text">
+                        <!-- <a class="nav-link active" aria-current="page" href="#">Voucher</a> -->
+                        <a class="nav-link disabled" href="#">Voucher</a>
+                    </li>
+                    <li class="nav-item text">
+                        <a class="nav-link disabled" href="#">Khuyến mãi</a>
+                    </li>
+                    <li class="nav-item text">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Thực đơn</a>
+                    </li>
+                    <li class="nav-item text">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Theo dõi đơn hàng</a>
+                    </li>
+                    <li class="nav-item text">
+                        <a class="nav-link active" aria-current="page" href="#">Danh sách khách hàng</a>
+                    </li>
+
+
+                </ul>
+                <button class="me-2 btn btn-success" style="border: 0px;"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                    </svg>Admin</button>
+
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
+</body>
+
+</html>
